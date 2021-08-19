d229f008e8a58a17806e57dc0ec49463cedf50c5
Siva Ranjani <sivaranjani.b@tvsnext.io>
2021-08-19 12:37:09 +0530

    [siva]Adding: DBAutoamtion_sql for validation

---
+++ DBAutomation.sql
@@ -0,0 +1,6 @@
+Select Top (1000) [FirstColumn],
+             [SecondColumn],
+			 [ThirdColumn],
+			 [fourthColumn]
+  from [DBAutomation].[dbo].[columns]
+  
---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+create database DBAutomation;
+use DBAutomation;
+create table columns
+(
+FirstColumn varchar (20),
+SecondColumn varchar (20),
+ThirdColumn varchar (20),
+FourthColumn varchar (20)
+)
